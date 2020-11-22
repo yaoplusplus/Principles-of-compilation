9666f325a001a372044368eaf2a8a0916d293b21
yaoplusplus <ljy5799@outlook.com>
2020-11-23 00:55:25 +0800

    improved codes from xuyang

---
+++ lab5/lab5-sample2 - 副本/src/tree.cpp
@@ -0,0 +1,133 @@
+#include "tree.h"
+void TreeNode::addChild(TreeNode* child) {
+    if(this->child==nullptr){
+        this->child=child;
+    }
+    else{
+        this->child->addSibling(child);
+    }
+}
+
+void TreeNode::addSibling(TreeNode* sibling){
+    TreeNode*currentsib=this->sibling;
+        while(currentsib!=nullptr){
+            currentsib=currentsib->sibling;
+        }
+        currentsib=sibling;
+}
+
+TreeNode::TreeNode(int lineno, NodeType type) {
+this->lineno=lineno;
+this->nodeType=type;
+}
+
+void TreeNode::genNodeId() {//按行遍历
+
+}
+
+void TreeNode::printNodeInfo() {
+        switch(this->nodeType){
+        case NODE_PROG:
+        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
+        <<" program";
+        this->printChildrenId();
+
+        case NODE_CONST:
+        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
+        <<" const type: "<<this->type->type;
+            break;
+
+        case NODE_VAR:
+        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
+        <<" variable varname: "<<this->var_name;
+            break;
+
+        case NODE_EXPR:
+         std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
+        <<" expression";
+        this->printChildrenId();
+            break;
+
+        case NODE_STMT:
+        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
+        <<" "<<this->nodeType;
+        this->printChildrenId();
+    //print statement type
+        std::cout<<" stmt:"<<this->type->type;
+            break;
+
+        case NODE_TYPE:
+        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
+        <<" type type:"<<this->type->getTypeInfo();
+            break;
+
+        default:
+            break;
+    }
+}
+
+void TreeNode::printChildrenId() {
+    if(this->child==nullptr)
+    return;
+    //child
+    else{
+        std::cout<<" children: [";
+        std::cout<<this->child->nodeID<<" ";
+        if(this->child->sibling=nullptr)
+        return;
+        //sibling
+        else{
+            std::cout<<this->child->sibling->nodeID<<" ";
+            TreeNode*currentsib=this->child->sibling->sibling;
+            while(currentsib!=nullptr)
+                {
+                    std::cout<<currentsib->nodeID<<" ";
+                    currentsib=currentsib->sibling;
+                }
+        }
+        std::cout<<" ]";
+    }
+}
+
+void TreeNode::printAST() {// father first
+    this->printNodeInfo();
+    this->child->printAST();
+    this->child->sibling->printAST();
+}
+
+// You can output more info...
+void TreeNode::printSpecialInfo() {
+    switch(this->nodeType){
+        case NODE_CONST:
+            break;
+        case NODE_VAR:
+            break;
+        case NODE_EXPR:
+            break;
+        case NODE_STMT:
+            break;
+        case NODE_TYPE:
+            break;
+        default:
+            break;
+    }
+}
+
+string TreeNode::sType2String(StmtType type) {
+    switch(type){
+        case STMT_SKIP:
+            return"skip";
+            break;
+        case STMT_DECL:
+            return "decl";
+            break;
+        default:
+            break;
+    }
+    return "?";
+}
+
+
+string TreeNode::nodeType2String (NodeType type){
+    return "<>";
+}
---
+++ lab5/lab5-sample2 - 副本/src/tree.h
@@ -0,0 +1,67 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "pch.h"
+#include "type.h"
+
+enum NodeType
+{
+    NODE_CONST, 
+    NODE_VAR,
+    NODE_EXPR,
+    NODE_TYPE, //节点的类型是类型
+
+    NODE_STMT,
+    NODE_PROG,
+};
+
+enum OperatorType
+{
+    OP_EQ,  // ==
+};
+
+enum StmtType {
+    STMT_SKIP,// 空语句
+    STMT_DECL,
+}
+;
+
+struct TreeNode {
+public:
+    int nodeID;  // 用于作业的序号输出
+    int lineno;
+    NodeType nodeType;
+
+    TreeNode* child = nullptr;
+    TreeNode* sibling = nullptr;
+
+    void addChild(TreeNode*);
+    void addSibling(TreeNode*);
+    
+    void printNodeInfo();
+    void printChildrenId();
+
+    void printAST(); // 先输出自己 + 孩子们的id；再依次让每个孩子输出AST。
+    void printSpecialInfo();
+
+    void genNodeId();
+
+public:
+    OperatorType optype;  // 如果是表达式
+    Type* type;  // 变量、类型、表达式结点，有类型。
+    StmtType stype;     //语句类型：跳转、循环、
+    int int_val;
+    char ch_val;
+    bool b_val;
+    string str_val;
+    string var_name;
+public:
+    static string nodeType2String (NodeType type);
+    static string opType2String (OperatorType type);
+    static string sType2String (StmtType type);
+
+public:
+    TreeNode(int lineno, NodeType type);
+};
+
+#endif
