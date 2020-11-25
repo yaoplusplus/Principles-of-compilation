e07ffcc7b37117350b2ac865a0242f70b160d312
yaoplusplus <ljy5799@outlook.com>
2020-11-25 13:31:19 +0800

    v1.0

--- lab5/lab5-sample2 - 副本/src/tree.cpp
+++ lab5/lab5-sample2 - 副本/src/tree.cpp
@@ -1,24 +1,30 @@
 #include "tree.h"
 void TreeNode::addChild(TreeNode* child) {
-    if(this->child==nullptr){
-        this->child=child;
+    if (this->child == nullptr) {
+        this->child = child;
     }
-    else{
+    else {
         this->child->addSibling(child);
     }
 }
 
-void TreeNode::addSibling(TreeNode* sibling){
-    TreeNode*currentsib=this->sibling;
-        while(currentsib!=nullptr){
-            currentsib=currentsib->sibling;
+void TreeNode::addSibling(TreeNode* sibling) {
+    TreeNode* currentsib = this->sibling;
+    if (currentsib == nullptr)
+        this->sibling = sibling;
+    else {
+        while (currentsib->sibling != nullptr) {
+
+            currentsib = currentsib->sibling;
         }
-        currentsib=sibling;
+        currentsib->sibling = sibling;
+    }
 }
 
 TreeNode::TreeNode(int lineno, NodeType type) {
-this->lineno=lineno;
-this->nodeType=type;
+    this->lineno = lineno;
+    this->nodeType = type;
+    this->nodeID = nodecount++;
 }
 
 void TreeNode::genNodeId() {//按行遍历
@@ -26,129 +32,155 @@ void TreeNode::genNodeId() {//按行遍历
 }
 
 void TreeNode::printNodeInfo() {
-        switch(this->nodeType){
-        case NODE_PROG:
-        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
-        <<" program";
+    if (this == nullptr)
+        return;
+    switch (this->nodeType) {
+    case NODE_PROG:
+        std::cout << "lno@" << this->lineno << "\t@" << this->nodeID
+            << " program";
         this->printChildrenId();
-            break;
-        case NODE_CONST:
-        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
-        <<" const type: "<<this->type->type;
-            break;
+        cout << endl;
+        break;
+    case NODE_CONST:
+        std::cout << "lno@" << this->lineno << "\t@" << this->nodeID
+            << " const type: " << this->type->type;
+        cout << endl;
+        break;
 
-        case NODE_VAR:
-        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
-        <<" variable varname: "<<this->var_name;
-            break;
+    case NODE_VAR:
+        std::cout << "lno@" << this->lineno << "\t@" << this->nodeID
+            << " variable varname: " << this->var_name;
+        cout << endl;
+        break;
 
-        case NODE_EXPR:
-         std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
-        <<" expression";
+    case NODE_EXPR:
+        std::cout << "lno@" << this->lineno << "\t@" << this->nodeID
+            << " expression";
         this->printChildrenId();
-            break;
+        cout << endl;
+        break;
 
-        case NODE_STMT:
-        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
-        <<" "<<this->nodeType;
+    case NODE_STMT:
+        std::cout << "lno@" << this->lineno << "\t@" << this->nodeID
+            << " " << "statement";
         this->printChildrenId();
-    //print statement type
-        std::cout<<" stmt:"<<this->type->type;
-            break;
+        //print statement type
+        // std::cout << " stmt:" << this->type->type;
+        cout << endl;
+        break;
 
-        case NODE_TYPE:
-        std::cout<<"lno@"<<this->lineno<<"\t@"<<this->nodeID
-        <<" type type:"<<this->type->getTypeInfo();
-            break;
+    case NODE_TYPE:
+        std::cout << "lno@" << this->lineno << "\t@" << this->nodeID
+            << " type type:" << this->type->getTypeInfo();
+        cout << endl;
+        break;
 
-        default:
-            break;
+    default:
+        break;
     }
 }
 
 void TreeNode::printChildrenId() {
-    if(this->child==nullptr)
-    return;
-    //child
-    else{
-        std::cout<<" children: [";
-        std::cout<<this->child->nodeID<<" ";
-        if(this->child->sibling==nullptr)
+    if (this->child == nullptr)
         return;
+    //child
+    else {
+        std::cout << " children: [";
+        std::cout << this->child->nodeID << " ";
+        if (this->child->sibling == nullptr)
+            return;
         //sibling
-        else{
-            std::cout<<this->child->sibling->nodeID<<" ";
-            TreeNode*currentsib=this->child->sibling->sibling;
-            while(currentsib!=nullptr)
-                {
-                    std::cout<<currentsib->nodeID<<" ";
-                    currentsib=currentsib->sibling;
-                }
+        else {
+            std::cout << this->child->sibling->nodeID << " ";
+            TreeNode* currentsib = this->child->sibling->sibling;
+            while (currentsib != nullptr)
+            {
+                std::cout << currentsib->nodeID << " ";
+                currentsib = currentsib->sibling;
+            }
         }
-        std::cout<<" ]";
+        std::cout << " ]";
     }
 }
 
 void TreeNode::printAST() {// father first
+    if (this == nullptr)
+        return;
     this->printNodeInfo();
+    if (this->child == nullptr)
+        return;
     this->child->printAST();
+    if (this->child->sibling == nullptr)
+        return;
     this->child->sibling->printAST();
 }
 
 // You can output more info...
 void TreeNode::printSpecialInfo() {
-    switch(this->nodeType){
-        case NODE_CONST:
-            break;
-        case NODE_VAR:
-            break;
-        case NODE_EXPR:
-            break;
-        case NODE_STMT:
-            break;
-        case NODE_TYPE:
-            break;
-        default:
-            break;
+    switch (this->nodeType) {
+    case NODE_CONST:
+        break;
+    case NODE_VAR:
+        break;
+    case NODE_EXPR:
+        break;
+    case NODE_STMT:
+        break;
+    case NODE_TYPE:
+        break;
+    default:
+        break;
     }
 }
 
 string TreeNode::sType2String(StmtType type) {
-    switch(type){
-        case STMT_SKIP:
-            return"skip";
-            break;
-        case STMT_DECL:
-            return "decl";
-            break;
-        default:
-            break;
+    switch (type) {
+    case STMT_SKIP:
+        return"skip";
+        break;
+    case STMT_DECL:
+        return "decl";
+        break;
+    default:
+        break;
     }
     return "?";
 }
 
-string TreeNode::nodeType2String (NodeType type){
-       switch(type){
-        case NODE_CONST:
-            return"const";
-            break;
-        case NODE_VAR:
-            return "variable";
-            break;
-        case NODE_EXPR:
-            return "expression";
-            break;
-        case NODE_TYPE:
-            return "type";
-            break;
-        case NODE_STMT:
-            return "statement";
-            break; 
-        case NODE_PROG:
-            return "program";
-            break;                                               
-        default:
+string TreeNode::nodeType2String(NodeType type) {
+    switch (type) {
+    case NODE_CONST:
+        return"const";
+        break;
+    case NODE_VAR:
+        return "variable";
+        break;
+    case NODE_EXPR:
+        return "expression";
+        break;
+    case NODE_TYPE:
+        return "type";
+        break;
+    case NODE_STMT:
+        return "statement";
+        break;
+    case NODE_PROG:
+        return "program";
+        break;
+    default:
         return"<>";
-            break;
+        break;
     }
 }
+
+// int main() {
+//     TreeNode* fa = new TreeNode(0,NODE_PROG);
+//     TreeNode* child= new TreeNode(1, NODE_STMT);
+//     TreeNode* sib1 = new TreeNode(2, NODE_STMT);
+//     TreeNode* sib2 = new TreeNode(3, NODE_STMT);
+//     fa->addChild(child);
+//     fa->addChild(sib1);
+//     fa->printNodeInfo();
+//     fa->printAST();
+//     return 0;
+// }
--- lab5/lab5-sample2 - 副本/src/tree.h
+++ lab5/lab5-sample2 - 副本/src/tree.h
@@ -10,21 +10,27 @@ enum NodeType
     NODE_VAR,
     NODE_EXPR,
     NODE_TYPE, //节点的类型是类型
-
     NODE_STMT,
     NODE_PROG,
 };
 
 enum OperatorType
 {
-    OP_EQ,  // ==
+    OP_EQ,      // ==
+    OP_PLUS,    // +
+    OP_MINUS,   //-
+    OP_MULTI,   // \*
 };
 
 enum StmtType {
     STMT_SKIP,// 空语句
     STMT_DECL,
+    STMT_IF,
+    STMT_LOOP,
 }
 ;
+
+static int nodecount=0;
 
 struct TreeNode {
 public:
@@ -47,14 +53,14 @@ public:
     void genNodeId();
 
 public:
-    OperatorType optype;  // 如果是表达式
-    Type* type;  // 变量、类型、表达式结点，有类型。
-    StmtType stype;     //语句类型：跳转、循环、
+    OperatorType optype;    // 如果是操作符节点:那么是什么操作符在此处
+    Type* type;             // NODE_TYPE :bool,int,...
+    StmtType stype;         //NODE_STMT 语句类型：跳转、循环、
     int int_val;
     char ch_val;
     bool b_val;
     string str_val;
-    string var_name;
+    string var_name;        //NODE_VAR
 public:
     static string nodeType2String (NodeType type);
     static string opType2String (OperatorType type);
