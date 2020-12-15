763e608068065e2d2062bd56de22069b7f3fb69f
yaoplusplus <ljy5799@outlook.com>
2020-12-15 22:25:42 +0800

    before debug : ID table

--- lab5/lab5-sample2 - 副本/src/layer.h
+++ lab5/lab5-sample2 - 副本/src/layer.h
@@ -25,7 +25,7 @@ struct layer{
     public:
     vector<variable*> vars;//每一个layer有一个var数组: 
     //实际上,每一个layer应该有多个var数组
-    //尝试修改这个bug,失败使用github desktop回溯版本到 before debug
+    //尝试修改这个bug,失败使用github desktop回溯版本到 'before debug'
     //两种方案:
     //1.将variable修改为数组,而不是单个元素.每一层有一个变量二维数组,每次进入
     //一个层级,都将符号放在一个新的var数组里(layers[i][i]).
